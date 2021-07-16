b3cee7d02f28fd6eabaed551fe01d09803b6a03e
Arnold Lee <arnoldlee60@gmail.com>
2021-07-15 23:05:04 -0500

    delete function should be good then finished

--- server.js
+++ server.js
@@ -55,10 +55,31 @@ function create(body, notesArray) {
     return newNote;
 }
 
+function remove(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+        //here
+    }
+}
+
 app.post('/api/notes', (req, res) => {
     const newNote = create(req.body, allNotes);
     res.json(newNote);
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    remove(req.params.id, allNotes);
+    res.json(true);
+});   
 
 
 app.listen(PORT, function() {
