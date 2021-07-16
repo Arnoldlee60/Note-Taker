075776a85282dc4864224733dade01e6985a2005
Arnold Lee <arnoldlee60@gmail.com>
2021-07-15 22:12:27 -0500

    create finished?

--- server.js
+++ server.js
@@ -17,22 +17,49 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+res.sendFile(path.join(__dirname, './public/index.html'));
 });
 /*
-require("./routes/apiRoute")(app);
+require("./routes/apiRoute")(app); //might be wrong?
 require("./routes/htmlRoute")(app);
 */
 //create
 //delete
+function create(body, notesArray) {
+    const newNote = body;
+
+    if (!Array.isArray(notesArray)){ //if not array
+        notesArray = [];
+    }
+
+    if (notesArray.length == 0){
+        notesArray.push(0);
+    }
+
+    body.id = notesArray[0];
+    notesArray[0]++; //increment
+    notesArray.push(newNote);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2) 
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = create(req.body, allNotes);
+    res.json(newNote);
+});
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
